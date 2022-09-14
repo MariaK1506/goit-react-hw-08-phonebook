82e8ace79e9c2595ba2f14d20c2bd7e6fabaa2c3
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-09-14 10:19:48 +0300

    fix warnings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import { GlobalStyle } from './GlobalStyle';
 import { ToastContainer } from 'react-toastify';
 import { Routes, Route } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { lazy } from 'react';
 import authOperations from '../redux/auth/auth-operations';
-import authSelectors from 'redux/auth/auth-selectors';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import { Container } from 'components/Container/Container';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
